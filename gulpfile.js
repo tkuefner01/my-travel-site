e8311911a44286db0a359230f9bf94323a626cfb
Thomas <tkuefner@gmail.com>
2018-01-01 18:38:47 +0100

    added files

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,3 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
-
-gulp.task('default', function(){
-	console.log("Hurrah, Gulp Task");
-});
-
-gulp.task('html', function(){
-	console.log("Hier passiert was mit dem HTML");
-});
-
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function(){
-	watch('./app/index.html', function(){
-		gulp.start('html');
-	});
-
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
-	})
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
